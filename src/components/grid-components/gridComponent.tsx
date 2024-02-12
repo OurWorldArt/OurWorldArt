386b51afaaea2ab1934d46f76212a48ba6094882
MarcBTHT <marc.berthol29@gmail.com>
2024-02-12 23:40:15 +0100

    feat: :triangular_flag_on_post: Modifying the selection cursor for the grid

--- src/components/grid-components/gridComponent.tsx
+++ src/components/grid-components/gridComponent.tsx
@@ -6,6 +6,7 @@ const GridComponent = () => {
     const gridWidth = 50; // Specify number of pixels horizontally
     const gridHeight = 50; // Specify number of pixels vertically
     const [selectedColor, setSelectedColor] = useState('#000000');
+    const [hoveredPixel, setHoveredPixel] = useState<PixelPosition | null>(null);
 
     // Create a matrix for the color grid
     const [gridColors, setGridColors] = useState(
@@ -27,6 +28,14 @@ const GridComponent = () => {
 
     const handlePixelClick = (rowIndex: number, colIndex: number) => {
         setCurrentPixel({ rowIndex, colIndex });
+        setHoveredPixel(null); // Add this to remove the hover effect after a click
+    };
+
+    const handlePixelHover = (rowIndex: number, colIndex: number) => {
+        // Only set hovered pixel if there is no currentPixel selected
+        if (!currentPixel) {
+            setHoveredPixel({ rowIndex, colIndex });
+        }
     };
 
     return (
@@ -35,14 +44,16 @@ const GridComponent = () => {
                 {gridColors.map((row, rowIndex) => (
                     <div key={rowIndex} className="flex">
                         {row.map((color, colIndex) => (
-                            <div
-                                key={`${rowIndex}-${colIndex}`}
-                                className="w-2 h-2 cursor-pointer" // Adjust w-2 h-2 to change pixel size
-                                style={{ backgroundColor: color }}
-                                onClick={() => handlePixelClick(rowIndex, colIndex)}
-                            />
-                        ))}
-                    </div>
+                        <div
+                            key={`${rowIndex}-${colIndex}`}
+                            className={`w-2 h-2 cursor-pointer ${hoveredPixel && hoveredPixel.rowIndex === rowIndex && hoveredPixel.colIndex === colIndex ? 'border-2 border-alice_blue' : ''} ${currentPixel && currentPixel.rowIndex === rowIndex && currentPixel.colIndex === colIndex ? 'border-2 border-old_rose' : ''}`}
+                            style={{ backgroundColor: color }}
+                            onMouseEnter={() => handlePixelHover(rowIndex, colIndex)}
+                            onMouseLeave={() => !currentPixel && setHoveredPixel(null)} // Only remove hover effect if no pixel is currently selected
+                            onClick={() => handlePixelClick(rowIndex, colIndex)}
+                        />
+                    ))}
+                </div>
                 ))}
             </div>
             {currentPixel !== null && (
