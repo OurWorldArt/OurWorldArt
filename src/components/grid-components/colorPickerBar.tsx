002550d93dd626001ccaadd09458930b85fb6ca5
MarcBTHT <marc.berthol29@gmail.com>
2024-02-17 00:17:43 +0100

    feat: :children_crossing: Disable the OK button once clicked

--- src/components/grid-components/colorPickerBar.tsx
+++ src/components/grid-components/colorPickerBar.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import { ColorPickerProps } from '../../types/index';
 
 const ColorPicker: React.FC<ColorPickerProps> = ({ selectedColor, onColorChange, onConfirm, onCancel }) => {
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const handleConfirm = () => { // Disable button once clicked
+        if (!isConfirmed) {
+            onConfirm();
+            setIsConfirmed(true);
+        }
+    };
+
     return (
         <div className="relative z-10 flex justify-center items-center space-x-2 p-2 bg-white rounded shadow-lg">
             <input
@@ -10,8 +20,9 @@ const ColorPicker: React.FC<ColorPickerProps> = ({ selectedColor, onColorChange,
                 className="w-16 h-8 cursor-pointer bg-white"
             />
             <button 
-                className="px-3 py-1 bg-old_rose text-white rounded shadow hover:bg-old_rose-400 focus:outline-none"
-                onClick={onConfirm}
+                className={`px-3 py-1 ${isConfirmed ? 'bg-gray-400' : 'bg-old_rose hover:bg-old_rose-400'} text-white rounded shadow focus:outline-none`}
+                onClick={handleConfirm}
+                disabled={isConfirmed} // Disable button once clicked
             >
                 OK
             </button>
